0429aa3615cc427979d05f5e650984866523094a
Charan Vignesh N R <charanvignesh10@gmail.com>
2023-08-15 21:27:13 +0530

    Very Basic REST API App

--- src/main/java/com/charan/learnspringboot/CourseController.java
+++ src/main/java/com/charan/learnspringboot/CourseController.java
@@ -1,4 +1,17 @@
 package com.charan.learnspringboot;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
 public class CourseController {
+    @RequestMapping(value = "/courses")
+    public List<Course> retrieveAllCourses() {
+        return Arrays.asList(new Course(1, "Spring Boot", "Charan"),
+                new Course(2, "DevOps", "Charan"),
+                new Course(3, "MERN", "Charan"));
+    }
 }
