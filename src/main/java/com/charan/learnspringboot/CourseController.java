cf3d9ffc174275941e37321f8ef6a9600c27a548
Charan Vignesh N R <charanvignesh10@gmail.com>
2023-08-16 20:14:29 +0530

    Creating JAR files

--- src/main/java/com/charan/learnspringboot/CourseController.java
+++ src/main/java/com/charan/learnspringboot/CourseController.java
@@ -3,18 +3,16 @@ package com.charan.learnspringboot;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.List;
-
 @RestController
 public class CourseController {
-    @RequestMapping(value = "/courses")
-    public List<Course> retrieveAllCourses() {
-        return Arrays.asList(new Course(1, "Spring Boot", "Charan"),
-                new Course(2, "DevOps", "Charan"),
-                new Course(3, "MERN", "Charan"),
-                new Course(4, "ML", "Charan"),
-                new Course(5, "Java", "Charan"));
+    private final CurrencyServiceConfiguration configuration;
 
+    public CourseController(CurrencyServiceConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
+    @RequestMapping(value = "/currency-service")
+    public CurrencyServiceConfiguration retrieveAllCourses() {
+        return configuration;
     }
 }
